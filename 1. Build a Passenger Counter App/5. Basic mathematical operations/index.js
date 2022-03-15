4a346a731057fd4a459b354787be2ac35e1ae295
Samson Teklehaimanot <samson.seged@gmail.com>
2022-03-15 16:23:59 +0300

    added two variables

--- 1. Build a Passenger Counter App/5. Basic mathematical operations/index.js
+++ 1. Build a Passenger Counter App/5. Basic mathematical operations/index.js
@@ -1,6 +1,13 @@
 // document.getElementById("count").innerText = 5
 
-// cmd+k+c
+
+let firstBatch = 5
+let secondBatch = 7
+
+let count = firstBatch + secondBatch
+
+console.log(count)
+
 // let firstBatch = 5
 // let secondBatch = 7
 // let count = firstBatch + secondBatch
@@ -8,6 +15,11 @@
 
 
 // 1. Create two variables, myAge and humanDogRatio
+
+let myAge = 32
+let humanDogRatio = 7
 // 2. Multiply the two together and store the result in myDogAge 
+let myDogAge = myAge * humanDogRatio
 // 3. Log myDogAge to the console
+console.log(myDogAge)
 
