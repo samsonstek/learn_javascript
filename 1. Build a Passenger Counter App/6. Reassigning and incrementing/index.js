4a00adffd61ec59e9db08d4e98d70e760ad7dce9
Samson Teklehaimanot <samson.seged@gmail.com>
2022-03-15 16:36:53 +0300

    created a variable and incremented by specific number

--- 1. Build a Passenger Counter App/6. Reassigning and incrementing/index.js
+++ 1. Build a Passenger Counter App/6. Reassigning and incrementing/index.js
@@ -2,9 +2,24 @@
 
 let count = 5
 
+count += 1
+
 console.log(count)
 
 // Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
 // Decrease it down to 25, and then finally increase it to 70
+
+let bonusPoints = 50
+console.log(bonusPoints)
+
+bonusPoints += 50
+console.log(bonusPoints)
+
+bonusPoints -= 75
+console.log(bonusPoints)
+
+bonusPoints += 45
+
+console.log(bonusPoints)
 
 
