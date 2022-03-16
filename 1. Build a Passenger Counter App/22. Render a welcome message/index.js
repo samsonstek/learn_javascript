df8bc1d87231fdff34fc37745a9e040d4df0d32e
Samson Teklehaimanot <samson.seged@gmail.com>
2022-03-16 18:03:16 +0300

    created two variables and render a welcome message

--- 1. Build a Passenger Counter App/22. Render a welcome message/index.js
+++ 1. Build a Passenger Counter App/22. Render a welcome message/index.js
@@ -1,6 +1,14 @@
 // Grab the welcome-el paragraph and store it in a variable called welcomeEl
+let welcomeEl = document.getElementById("welcome-el")
 
 // Create two variables (name & greeting) that contains your name
+let myName = "sami"
+let greeting = "welcome back, "
+
 // and the greeting we want to render on the page
 
+let myGreeting = greeting + myName
+
 // Render the welcome message using welcomeEl.innerText
+
+console.log(welcomeEl.innerText=myGreeting)
