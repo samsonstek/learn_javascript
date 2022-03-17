135873209f41254cbefbd19800ce09eaef31fa2f
Samson Teklehaimanot <samson.seged@gmail.com>
2022-03-17 14:22:07 +0300

    concatenate two strings in a function

--- 2. Practice time - part 1/2. Contatenate two strings in a function/index.js
+++ 2. Practice time - part 1/2. Contatenate two strings in a function/index.js
@@ -1,5 +1,11 @@
-let name = "Linda"
+let myName = "Linda"
 let greeting = "Hi there"
 
 // Create a function that logs out "Hi there, Linda!" when called
 
+function myGreeting() {
+    console.log(greeting + "," + myName +"!")
+
+}
+
+myGreeting()
