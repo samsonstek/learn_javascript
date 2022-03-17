2de50490f4683af0c09d889c82d4200e642f9852
Samson Teklehaimanot <samson.seged@gmail.com>
2022-03-17 12:37:09 +0300

    added a reset button and when pressed it sets counter to 0

--- 1. Build a Passenger Counter App/27. Set the count to 0/index.js
+++ 1. Build a Passenger Counter App/27. Set the count to 0/index.js
@@ -1,5 +1,6 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
+
 let count = 0
 
 function increment() {
@@ -14,3 +15,7 @@ function save() {
     count = 0
 }
 
+function reset() {
+    countEl.textContent = 0
+    count = 0
+}
