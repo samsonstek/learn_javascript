3d59d1bd208e5c4009f6847110a39868ea311cd4
Samson Teklehaimanot <samson.seged@gmail.com>
2022-03-17 18:17:44 +0300

    added if...else conditional

--- 3. Build a Blackjack game/3. If...else conditionals/index.js
+++ 3. Build a Blackjack game/3. If...else conditionals/index.js
@@ -1,4 +1,11 @@
-let firstCard = 6
-let secondCard = 9
+let firstCard = 10
+let secondCard = 12
 let sum = firstCard + secondCard
 
+if (sum < 21) {
+    console.log("Do you want to draw a new card?")
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack!")
+} else {
+    console.log("You are out of the game!")
+}
