082ec800cbdc7c6575fe48d3b1a79ab7f95293b1
Samson Teklehaimanot <samson.seged@gmail.com>
2022-03-17 17:20:19 +0300

    Made a calculator that can Add, Subtract, Divide and Multiply

--- 2. Practice time - part 1/6. Calculator challenge/index.js
+++ 2. Practice time - part 1/6. Calculator challenge/index.js
@@ -1,7 +1,29 @@
-let num1 = 8
+let num1 = 10
 let num2 = 2
 document.getElementById("num1-el").textContent = num1
 document.getElementById("num2-el").textContent = num2
+
+let sumEl = document.getElementById("sum-el")
+
+function add() {
+    result = num1 + num2
+    sumEl.textContent = "Sum: " + result
+}
+
+function subtract() {
+    result = num1 - num2
+    sumEl.textContent = "Difference: " + result
+}
+
+function divide() {
+    result = num1 / num2
+    sumEl.textContent = "Division: " + result
+}
+
+function multiply() {
+    result = num1 * num2
+    sumEl.textContent = "Product: " + result
+}
 
 // Create four functions: add(), subtract(), divide(), multiply()
 // Call the correct function when the user clicks on one of the buttons
