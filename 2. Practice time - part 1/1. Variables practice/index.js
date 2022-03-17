d22b5985712960be3a8b69e5b2408f23378aad98
Samson Teklehaimanot <samson.seged@gmail.com>
2022-03-17 14:15:50 +0300

    create two variables and concatenate them

--- 2. Practice time - part 1/1. Variables practice/index.js
+++ 2. Practice time - part 1/1. Variables practice/index.js
@@ -1,7 +1,11 @@
 // Create two variables, firstName and lastName
+let firstName = "sami"
+let lastName = "best"
 
 // Concatenate the two variables into a third variable called fullName
+let fullName = firstName + " " + lastName
 
 // Log fullName to the console
+console.log(fullName)
 
 
