f056f4e4039817cf088268acd3a57f23d0f854ec
Samson Teklehaimanot <samson.seged@gmail.com>
2022-03-17 15:12:34 +0300

    Rendering an error message when button is clicked

--- 2. Practice time - part 1/5. Rendering an error message/index.js
+++ 2. Practice time - part 1/5. Rendering an error message/index.js
@@ -2,4 +2,12 @@
 // "Something went wrong, please try again" in the paragraph
 // that has the id="error".
 
+let errorParagraph = document.getElementById("error")
+console.log(errorParagraph)
 
+
+function purchase() {
+    console.log("Button Clicked")
+    errorParagraph.textContent = "Something went wrong, please try again"
+
+}
