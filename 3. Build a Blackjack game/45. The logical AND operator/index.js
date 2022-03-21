ba4fd44dfae3e3d469c5abb79ddb387e11bd109d
Samson Teklehaimanot <samson.seged@gmail.com>
2022-03-21 17:02:58 +0300

    Using the AND operator

--- 3. Build a Blackjack game/45. The logical AND operator/index.js
+++ 3. Build a Blackjack game/45. The logical AND operator/index.js
@@ -1,11 +1,10 @@
-let hasCompletedCourse = false
+let hasCompletedCourse = true
 let givesCertificate = true
 
-if (hasCompletedCourse === true) {
-    if (givesCertificate === true) {
-        generateCertificate()
-    }
+if (hasCompletedCourse === true && givesCertificate === true) {
+    generateCertificate()
 }
+
 
 function generateCertificate() {
     console.log("Generating certificate....")
