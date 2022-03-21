bc3fd03a3484d00a1308d33b2a275c70768ce9b3
Samson Teklehaimanot <samson.seged@gmail.com>
2022-03-21 14:55:31 +0300

    Returning values in functions

--- 3. Build a Blackjack game/34. Returning values in functions/index.js
+++ 3. Build a Blackjack game/34. Returning values in functions/index.js
@@ -12,12 +12,16 @@ function getFastestRaceTime() {
     }
 }
 
-// let fastestRace = getFastestRaceTime()
-// console.log(fastestRace)
+let fastestRace = getFastestRaceTime()
+console.log(fastestRace)
 
 // Write a function that returns the total race time
 // Call/invoke the function and store the returned value in a new variable
 // Finally, log the variable out
 
+function totalRaceTime () {
+    return player1Time + player2Time
+}
 
-
+let totalTime = totalRaceTime()
+console.log(totalTime)
